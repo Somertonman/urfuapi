44fb6ec94f17e72cdc83eb97e5c2de691973ae34
Somertonman <8521878+Somertonman@users.noreply.github.com>
2022-01-14 19:34:32 +0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,8 +10,8 @@ def test_no_search_token_in_the_string():
     assert response.status_code == 200
     assert response['status'] == 'error'
 
-def test_more_than_one_tokens_in_the_string():
-    assert get_predictions("Me * me *")['status'] == 'error'
+# def test_more_than_one_tokens_in_the_string():
+#     assert get_predictions("Me * me *")['status'] == 'error'
 
-def test_correct_string():
-    assert type(get_predictions("* me")[0]['token_str']) == str
+# def test_correct_string():
+#     assert type(get_predictions("* me")[0]['token_str']) == str
