7974f3bb276b592c2c7337bdcc5eab58d6d74689
Somertonman <8521878+Somertonman@users.noreply.github.com>
2022-01-14 19:32:49 +0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,14 @@
-from main import get_predictions
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
 
 def test_no_search_token_in_the_string():
-    assert get_predictions("Meme")['status'] == 'error'
+    response = client.post("/get_suggestions/",
+        json={"Meme"}
+    )
+    assert response.status_code == 200
+    assert response['status'] == 'error'
 
 def test_more_than_one_tokens_in_the_string():
     assert get_predictions("Me * me *")['status'] == 'error'
