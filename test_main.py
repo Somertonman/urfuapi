07c71cfc7b3252d3199231684194124ac83888bd
Somertonman <8521878+Somertonman@users.noreply.github.com>
2022-01-20 17:19:37 +0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -21,5 +21,21 @@ def test_correct_string():
         json={"sentence":"* me"}
     )
     assert response.status_code == 200
-    
     assert response.json()['data'][0]['sequence'] == 'tell me'
+
+def test_correct_error_min_score():
+    
+    response = client.post("/get_suggestions",
+        json={"sentence":"I * horses", "min_score": 0.7})
+    )
+    assert response.status_code == 200
+    assert response.json()['details'] == 'No results to return. Probably min_score is too high.'
+    
+def test_correct_error_n_of_results():
+    
+    response = client.post("/get_suggestions",
+        json={"sentence":"Me * me", "n_of_results": 3}
+    )
+    assert response.status_code == 200
+    assert len (response.json()['data']) == 3   
+   
